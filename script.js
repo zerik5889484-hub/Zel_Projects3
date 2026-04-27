d9056613ed0b967516b074fb4cdc981ae2859d92
zEL <zerik5889484@gmail.com>
2026-04-27 16:00:05 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    document.getElementById("loader").style.display = "none";
+  }, 800); // быстрее загрузка
+});
+
+document.getElementById("startBtn").addEventListener("click", () => {
+  document.getElementById("start").style.display = "none";
+  document.getElementById("menu").style.display = "flex";
+  document.getElementById("home").classList.remove("hidden");
+});
+
+document.querySelectorAll("nav a").forEach(link => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    document.querySelectorAll(".page").forEach(p => p.classList.add("hidden"));
+
+    const target = link.getAttribute("href").replace("#", "");
+    document.getElementById(target).classList.remove("hidden");
+  });
+});
